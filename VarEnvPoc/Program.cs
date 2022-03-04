545b047fa590928663ba35307bb45611335aea41
marco luis hung campos <marco.luis.hung@external.wolterskluwer.com>
2022-03-04 09:54:57 +0100

    Init

---
+++ VarEnvPoc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace VarEnvPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+            Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+            Console.WriteLine($"ASPNETCORE_ENVIRONMENT = {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
+            Console.WriteLine($"KeyOne = {settings.KeyOne}");
+            Console.WriteLine($"KeyTwo = {settings.KeyTwo}");
+            Console.WriteLine($"KeyThree:Message = {settings.KeyThree.Message}");
+        }
+    }
+}
---
+++ VarEnvPoc/Settings.cs
@@ -0,0 +1,14 @@
+namespace VarEnvPoc
+{
+    public class Settings
+    {
+        public int KeyOne { get; set; }
+        public bool KeyTwo { get; set; }
+        public NestedSettings KeyThree { get; set; } = null!;
+    }
+
+    public class NestedSettings
+    {
+        public string Message { get; set; } = null!;
+    }
+}
